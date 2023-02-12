04d4733b5ebef5ea8839a51dafb984d5181ceb5f
bhavnagoyal <95182361+bhavnagoyal@users.noreply.github.com>
2023-02-13 02:04:54 +0530

    Update mashup.py

--- mashup.py
+++ mashup.py
@@ -1,4 +1,5 @@
 import smtplib
+import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
@@ -38,25 +39,25 @@ def get_links(query):
 # for link in links:
 #     print(link)
 
-def download_video(link, folder):
+def download_video(link):
     yt = YouTube(link)
     stream = yt.streams.first()
-    stream.download(folder)
+    stream.download()
     print("Video downloaded successfully")
     
-def convert_to_audio(folder):
-    for filename in os.listdir(folder):
+def convert_to_audio():
+    for filename in os.listdir():
         if filename.endswith(".3gpp"):
-            video = VideoFileClip(os.path.join(folder, filename))
+            video = VideoFileClip(os.path.join(filename))
             audio = video.audio
-            audio.write_audiofile(os.path.join(folder, filename.split(".")[0] + ".mp3"))
+            audio.write_audiofile(os.path.join(filename.split(".")[0] + ".mp3"))
             print("Converted {} to audio successfully".format(filename))
             
-def cut_audio(folder, seconds):
-    for audio_file in os.listdir(folder):
+def cut_audio(seconds):
+    for audio_file in os.listdir():
         if audio_file.endswith(".mp3"):
-            audio_path = os.path.join(folder, audio_file)
-            cut_audio_path = os.path.join(folder, audio_file)
+            audio_path = os.path.join(audio_file)
+            cut_audio_path = os.path.join(audio_file)
 
         # Load the audio file
             audio = AudioFileClip(audio_path)
@@ -68,20 +69,25 @@ def cut_audio(folder, seconds):
         # Save the cut audio to a file
             cut_audio.write_audiofile(cut_audio_path)
             
-def merge_audio(folder, output_filename):
-    audio_clips = []
-    for filename in os.listdir(folder):
-        if filename.endswith(".mp3") or filename.endswith(".wav"):
-            audio_clips.append(AudioFileClip(os.path.join(folder, filename)))
-    final_audio = concatenate_audioclips(audio_clips)
-    final_audio.write_audiofile(output_filename)
-    print("Merged all audio files in the folder successfully")
-    return output_filename
+# def merge_audio():
+#     audio_clips = []
+#     for filename in os.listdir():
+#         if filename.endswith(".mp3") or filename.endswith(".wav"):
+#             audio_clips.append(AudioFileClip(os.path.join(filename)))
+#     final_audio = concatenate_audioclips(audio_clips)
+#     final_audio.write_audiofile()
+#     print("Merged all audio files in the folder successfully")
+def merge_audios():
+    audio_files = [AudioFileClip(file) for file in os.listdir(os.getcwd()) if file.endswith('.mp3')]
+    final_audio = concatenate_audioclips(audio_files)
+    final_audio.write_audiofile("final_audio.mp3")
+    return "final_audio.mp3"
+    
 
 
 def send_email(to, audio_filename):
-    from_email = "kaamkartechlo5050@gmail.com"
-    from_password = "fsxjjjwvmklfnzfw"
+    from_email = "bhavgoyal105020@gmail.com"
+    from_password = "wpggwxconjwezqbr"
     subject = "Audio file"
     message = "Attached is the audio file."
 
@@ -89,7 +95,7 @@ def send_email(to, audio_filename):
     msg["From"] = from_email
     msg["To"] = to
     msg["Subject"] = subject
-    msg.attach(MIMEText(message, "plain"))
+    msg.attach(MIMEText(message,"plain"))
 
     with open(audio_filename, "rb") as f:
         audio = MIMEAudio(f.read(),_subtype='wav')
@@ -102,35 +108,38 @@ def send_email(to, audio_filename):
     server.sendmail(from_email, to, msg.as_string())
     server.quit()
 
-def folder1(folder,links):
-    if not os.path.isdir(folder):
-        os.makedirs(folder)
+def folder1(links):
+    # if not os.path.isdir(folder):
+    #     os.makedirs(folder)
     for link in links:
-        download_video(link, folder)
+        download_video(link)
 
+def send_request():
+    # code to send the request
+        st.title("Audio File Cutter")
+        st.write("Enter the name of the singer, the number of seconds to cut, and your email address")
+        #folder = "vid"
+        singer = st.text_input("Singer name: ")
+        seconds = st.number_input("Number of seconds to cut: ")
+        num = st.number_input("Number of videos: ",min_value=2, max_value=50, step=1)
+        start_or_end = "start"
+        to = st.text_input("Email address: ")
 
-def main():
-    st.title("Audio File Cutter")
-    st.write("Enter the name of the singer, the number of seconds to cut, and your email address")
-    folder = "vid"
-    singer = st.text_input("Singer name: ")
-    seconds = st.number_input("Number of seconds to cut: ")
-    num = st.number_input("Number of videos: ",min_value=10, max_value=50, step=1)
-    start_or_end = "start"
-    to = st.text_input("Email address: ")
-
-    if st.button("Submit"):
-        links= get_links(singer)[:num]
-        folder1(folder,links)
-        convert_to_audio(folder)
-        cut_audio(folder, seconds)
-        output_filename=merge_audio(folder, "outfile.mp3")
-        #output_filename="outfile.mp3"
-        if output_filename:
-            send_email(to, output_filename)
-            st.success("Audio file sent to your email address successfully")
-        else:
-            st.error("No audio file found for the singer")
-
-if __name__ == "__main__":
-    main()
+        if st.button("Submit"):
+            links= get_links(singer)[:num]
+            folder1(links)
+            convert_to_audio()
+            cut_audio( seconds)
+            output_filename=merge_audios()
+            #output_filename="outfile.mp3"
+            if output_filename:
+                send_email(to, output_filename)
+                st.success("Audio file sent to your email address successfully")
+            else:
+                st.error("No audio file found for the singer")
+                
+rate_limit = 2 # requests per second
+time_between_requests = 1 / rate_limit
+while True:
+    send_request()
+    time.sleep(time_between_requests)
